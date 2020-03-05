2bd26a806e4040a2bb3c43212933af229964ef4b
NBK-17-120\User <karyann@apdgroup.com>
2020-03-05 19:15:28 +0800

    update cherrypick

--- src/js/components/travel-form.js
+++ src/js/components/travel-form.js
@@ -414,4 +414,19 @@ $(document).ready(function () {
     }
 
 
+    //changes the footer to fix only in homepage
+    if ($('.homepage-wrapper').length) {
+        $(".footer-wrapper").css('position', 'fixed');
+    }
+
+    //nav menu
+    $(".hamburger-menu").click(function () {
+        $('#newNavMenu').addClass('showMenu');
+    });
+    
+    $(".nav-menu .btn-close").click(function () {
+        $('#newNavMenu').removeClass('showMenu');
+    });
+    
+
 });
